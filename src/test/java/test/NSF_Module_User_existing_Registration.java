99a04ad01021923dba510018a6c098507750b3cb
Ajay Thakur <ajaythakur63@gmail.com>
2026-07-08 18:07:21 +0530

    manual Registartion

---
+++ src/test/java/test/NSF_Module_User_existing_Registration.java
@@ -0,0 +1,33 @@
+package test;
+
+import org.testng.annotations.Test;
+import PageObject.GTCC_Admin_Page;
+import PageObject.NSF_Module_Admin;
+import utilities.BaseClass;
+import utilities.RandomeData;
+
+public class NSF_Module_User_existing_Registration  extends BaseClass{
+	
+	
+	
+String Searchname=	RandomeData.TwoDigitString();
+	
+	@Test(priority=1)
+	void Module_User_registration() throws InterruptedException {
+	GTCC_Admin_Page page= new GTCC_Admin_Page(driver);
+	page.loginNSFModuleUser();
+	NSF_Module_Admin pa=new NSF_Module_Admin(driver);
+	pa.TeamNomintion();
+	Thread.sleep(1000);
+	pa.StartEntery();
+	Thread.sleep(1000);
+	pa.SearchEntery();
+	Thread.sleep(1000);
+	}
+	//@Test(priority=2)
+	void manual_Registartion() {
+		manual_Registartion();
+		
+	}
+
+}
--- src/test/java/test/NSF_Module_User_manual_Registration.java
+++ src/test/java/test/NSF_Module_User_manual_Registration.java
@@ -6,23 +6,31 @@ import PageObject.NSF_Module_Admin;
 import utilities.BaseClass;
 import utilities.RandomeData;
 
-public class NSF_Module_User_Start_Registration  extends BaseClass{
+public class NSF_Module_User_manual_Registration  extends BaseClass{
 	
 	
 	
 String Searchname=	RandomeData.TwoDigitString();
+String FirstName=RandomeData.randomString();
+String MiddleName=RandomeData.randomString();
+String LastName=RandomeData.randomString();
+String Email=RandomeData.randomString()+"@yopmail.com";
+
 	
-	@Test
+	@Test(priority=1)
 	void Module_User_registration() throws InterruptedException {
 	GTCC_Admin_Page page= new GTCC_Admin_Page(driver);
 	page.loginNSFModuleUser();
 	NSF_Module_Admin pa=new NSF_Module_Admin(driver);
 	pa.TeamNomintion();
-	Thread.sleep(1000);
 	pa.StartEntery();
+	pa.manualRegistartion();
 	Thread.sleep(1000);
-	pa.SearchEntery();
-	Thread.sleep(1000);
+	pa.NameSelection(FirstName, MiddleName, LastName);
+	Thread.sleep(2000);
+	pa.DOB_Email(Email);
+	Thread.sleep(2000);
+	
 	}
 
 }
